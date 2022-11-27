156686ad660c899f2f6a19123e5c9aac368888a7
Muhammad Rizky Fadhillah <kikimuhammad69@gmail.com>
2022-11-27 20:01:25 +0700

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// Style & Bootstrap
+import './styles/style.scss';
+
+// For Bootstrap functionality
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
