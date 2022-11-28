f2bc08b9d496b039987e4e769cda86dbe7e4f93c
Dias Nur Ramadhan <diasnurramadhan@gmail.com>
2022-11-28 15:10:30 +0700

    add Overview

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import Navbar from './component/Navbar/Navbar';
+import Overview from './component/Overview/Overview';
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Overview />
+    </>
+  );
 }
 
 export default App;
---
+++ src/component/Overview/Overview.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Overview.scss';
+
+function Overview() {
+  return (
+    <div className="container my-bg">
+      <div className="row d-flex justify-content-center align-items-center overview mt-lg-5">
+        <div className="col-lg-6 my-3 d-flex flex-column justify-content-center align-items-center justify-content-md-start align-items-md-start text-center text-md-start">
+          <div className="mental-health d-flex justify-content-between align-items-center">
+            Mental Health <img src="./img/heart.svg" alt="heart" />
+          </div>
+          <h1 className="title">
+            <span className="biru">Sebuah cara mudah</span>
+            <br /> mengatasi masalah <br /> kesehatan mentalmu
+          </h1>
+          <p className="description">
+            Kesehatan mental merupakan hal penting bagi setiap orang, kesehatan mental yang buruk
+            dapat mengganggu kehidupanmu, itu sangat tidak keren. Aplikasi ini adalah sahabat kamu
+            sekarang, brodie~
+          </p>
+          <a href="#" className="my-btn mt-3">
+            Daftar Sekarang
+          </a>
+        </div>
+
+        <div className="col-lg-6 d-flex justify-content-center align-items-center">
+          <img src="./img/vector-kesehatan.png" alt="" className="vector-kesehatan" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Overview;
