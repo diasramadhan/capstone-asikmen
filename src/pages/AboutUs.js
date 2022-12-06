67209fdb6787e6c18f3f967006fd993442614666
Nugroho <odeoofficial@gmail.com>
2022-12-06 21:09:28 +0700

    update

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
 import AboutContent from '../components/Main_About/AboutContent';
 import AboutProfile from '../components/Main_About/AboutProfile';
-import Footer from '../components/Footer/Footer';
 
 function AboutUs() {
   return (
     <>
-      <Navbar />
       <AboutContent />
       <AboutProfile />
-      <Footer />
     </>
   );
 }
